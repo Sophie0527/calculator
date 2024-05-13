88821c4476a68d3d158146b097b55c8aa8fbd415
손소희 <ssh30510044@gmail.com>
2024-05-13 19:07:42 +0900

    06. 예외 처리

--- src/Calculator.js
+++ src/Calculator.js
@@ -45,11 +45,19 @@ export default () => {
   // 값이 나온 이후('='을 선택한 이후)
   const [tempInput, setTempInput] = useState(null); //마자막 input의 숫자
   const [tempOperator, setTempOperator] = useState(null); //마자막 선택한 기호
+  // 바로 직전에 연산자를 클릭 했는지 여부
+  const [isClickedOperator, setIsClickedOperator] = useState(false);
+  // 직전에 '='를 눌렀는지 여부
+  const [isClickedEqual, setIsClickedEqual] = useState(false);
+
+  // input 값이 있는지 없는지 여부
+  const hasInput = !!input; // 같은 의미: const hasInput = input ? true : false;
 
   const onPressNum = (num) => {
-    if (currentOperator) {
+    if (currentOperator && isClickedOperator) {
       setResult(input);
       setInput(num);
+      setIsClickedOperator(false);
     } else {
       const newInput = Number(`${input}${num}`);
       setInput(newInput);
@@ -59,35 +67,47 @@ export default () => {
   const onPressOperator = (operator) => {
     if (operator !== '=') {
       setCurrentOperator(operator);
+      setIsClickedOperator(true);
+      setIsClickedEqual(false);
     } else {
       let finalResult = result;
-      switch (currentOperator) {
+      const finalInput = isClickedEqual ? tempInput : input;
+      const finalOperator = isClickedEqual ? tempOperator : currentOperator;
+      switch (finalOperator) {
         case '+':
-          finalResult = result + input;
+          finalResult = result + finalInput;
           break;
         case '-':
-          finalResult = result - input;
+          finalResult = result - finalInput;
           break;
         case '*':
-          finalResult = result * input;
+          finalResult = result * finalInput;
           break;
         case '/':
-          finalResult = result / input;
+          finalResult = result / finalInput;
           break;
         default:
           break;
       }
       setResult(finalResult);
       setInput(finalResult);
+      setTempInput(finalInput);
+      setCurrentOperator(null);
+      setTempOperator(finalOperator);
+      setIsClickedEqual(true);
     }
   };
 
   const onPressReset = () => {
-    setInput(0);
-    setCurrentOperator(null);
-    setResult(null);
-    setTempInput(null);
-    setTempOperator(null);
+    if (hasInput) {
+      setInput(0);
+    } else {
+      setInput(0);
+      setCurrentOperator(null);
+      setResult(null);
+      setTempInput(null);
+      setTempOperator(null);
+    }
   };
 
   return (
@@ -107,7 +127,12 @@ export default () => {
 
       {/* [AC ~ /] */}
       <ButtonContainer>
-        <Button type="reset" text="AC" onPress={onPressReset} flex={3} />
+        <Button
+          type="reset"
+          text={hasInput ? 'C' : 'AC'}
+          onPress={onPressReset}
+          flex={3}
+        />
         <Button
           type="operator"
           text="/"
